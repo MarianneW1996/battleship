b03ebbda34739c29f48f98ff3d9fe2a0a7ee145f
Marianne Wihsbeck <marianne.wihsbeck@a1.net>
2025-01-03 21:27:42 +0100

    user_selection_added

--- battleship.py
+++ battleship.py
@@ -8,4 +8,24 @@ def create_grid(coordinates: list):
             grid[x][y] = 'X'
         print (grid[i])
     return grid
-create_grid(example_grid)
+
+# user selection
+def user_selection():
+    coordinates = []
+    number_ships_max = 20
+    number_ships_used = 0
+    while number_ships_used <= number_ships_max:
+        try:
+            selection_x = int(input("Position x: (0-9):"))
+            selection_y = int(input("Position y: (0-9):"))
+            selected_position = (selection_x, selection_y)
+            if not (0 <= selection_x <= 9 and 0 <= selection_y <= 9):
+                print("Position outside if the grid. Please enter values between 0 and 9!")
+            elif selected_position not in coordinates:
+                coordinates.append((selection_x, selection_y))
+                number_ships_used += 1
+            else:
+                print("Position is already occupied. Please enter an new position.")
+        except ValueError:
+            print("Please enter values between 0 and 9!")
+    return create_grid(coordinates)
