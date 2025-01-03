2b84977ca72ddd63e12346b66af85e3215127341
Marianne Wihsbeck <marianne.wihsbeck@a1.net>
2025-01-03 16:09:59 +0100

    create_grid_print_added

--- battleship.py
+++ battleship.py
@@ -2,8 +2,10 @@ example_grid = ([(0,0),(4,5),(6,2),(9,4)])
 
 # create a grid
 def create_grid(coordinates: list):
- grid = [["." for x in range(10)] for y in range(10)]
- for x,y in coordinates:
-    grid[x][y] = 'X'
- return grid
-print(create_grid(example_grid))
+    grid = [["." for x in range(10)] for y in range(10)]
+    for i in range(len(grid)):
+        for x,y in coordinates:
+            grid[x][y] = 'X'
+        print (grid[i])
+    return grid
+create_grid(example_grid)
